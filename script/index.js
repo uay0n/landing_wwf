b63e101c509188be208f4e89330ea854e7acf21f
uay0n <hoshe1717@gmail.com>
2026-07-23 15:32:24 +0900

    북극곰 4행완성

--- script/index.js
+++ script/index.js
@@ -6,10 +6,11 @@
 const popup_box = document.querySelector('.answer_box')
 const popup_bg = document.querySelector('.answer_popup_bg')
 const quiz_btn = document.querySelector('#quiz_btn')
-const close_btn = document.querySelector('#quiz_btn')
+const close_btn = document.querySelector('#answer_close_btn')
 console.log(popup_box);
 console.log(popup_bg);
 console.log(quiz_btn);
+console.log(close_btn)
 
 popup_bg.style.display = 'none'
 
@@ -18,10 +19,13 @@ popup_bg.style.display = 'none'
 // addEventListener 이벤트를 호출하는 함수
 // addEventListener ('이벤트종류',실행함수)
 quiz_btn.addEventListener('click',popupShow);
+close_btn.addEventListener('click',popupDown)
 function popupShow(){
-    popup_bg.style.display = 'block'
+    popup_bg.style.display = 'flex'
 }
-
+function popupDown(){
+    popup_bg.style.display = 'none'
+}
 /* function popupShow(){
     popup_box.style.display = 'flex';
     popup_bg.style.display = 'flex';
